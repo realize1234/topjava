fc3c05f8cb0c6fa3a45b763b94f1ee48e00f5b1c
jeromnik <jeromnik2@gmail.com>
2018-08-31 19:30:26 +0300

    Meal entity

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,33 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+@Entity
+@Table(name = "meals", uniqueConstraints =
+        {@UniqueConstraint(columnNames ={"date_time", "user_id"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends BaseEntity {
+    @Column(name = "date_time", nullable = false, columnDefinition = "timestamp default now()")
+    @NotNull @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotNull @Range(min = 10, max = 3000)
     private int calories;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Meal() {
