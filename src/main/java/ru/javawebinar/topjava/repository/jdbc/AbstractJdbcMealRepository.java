bf0268616e3de6fa441d4eb9933fef15e5c46eb1
jeromnik <jeromnik2@gmail.com>
2018-09-02 18:12:54 +0300

    5 4(Optional) - jdbcTimeStamp for HSQL_DB profile

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepository.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,33 +10,58 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.sql.DataSource;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public class JdbcMealRepositoryImpl implements MealRepository {
-
-    private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
-
-    private final JdbcTemplate jdbcTemplate;
-
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    private final SimpleJdbcInsert insertMeal;
+public abstract class AbstractJdbcMealRepository<TimeType> implements MealRepository {
+   private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
 
     @Autowired
-    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+    private  JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private  NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private  SimpleJdbcInsert insertMeal;
+
+    protected abstract TimeType toDataSourceTime(LocalDateTime localDateTime);
+    @Autowired
+    public void setDateSource(DataSource dataSource) {
         this.insertMeal = new SimpleJdbcInsert(dataSource)
                 .withTableName("meals")
                 .usingGeneratedKeyColumns("id");
-
-        this.jdbcTemplate = jdbcTemplate;
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
+
+
+    @Repository
+    @Profile(Profiles.POSTGRES_DB)
+    public static class JdbcLocalDateTimeMealRepository extends AbstractJdbcMealRepository<LocalDateTime>  {
+
+        @Override
+        protected LocalDateTime toDataSourceTime(LocalDateTime localDateTime) {
+            return localDateTime;
+        }
+    }
+
+
+    @Repository
+    @Profile(Profiles.HSQL_DB)
+    public static class JdbcTimeStampMealRepository extends AbstractJdbcMealRepository<Timestamp> {
+
+        @Override
+        protected Timestamp toDataSourceTime(LocalDateTime localDateTime) {
+            return Timestamp.valueOf(localDateTime);
+        }
+    }
+
+
 
     @Override
     public Meal save(Meal meal, int userId) {
@@ -43,7 +69,7 @@ public class JdbcMealRepositoryImpl implements MealRepository {
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", toDataSourceTime(meal.getDateTime()))
                 .addValue("user_id", userId);
 
         if (meal.isNew()) {
@@ -83,6 +109,6 @@ public class JdbcMealRepositoryImpl implements MealRepository {
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
+                ROW_MAPPER, userId, toDataSourceTime(startDate), toDataSourceTime(endDate));
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/meal/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/meal/JdbcMealServiceTest.java
@@ -3,8 +3,9 @@ package ru.javawebinar.topjava.service.meal;
 import org.springframework.test.context.ActiveProfiles;
 
 import static ru.javawebinar.topjava.Profiles.ACTIVE_DB;
+import static ru.javawebinar.topjava.Profiles.HSQL_DB;
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
-@ActiveProfiles({ACTIVE_DB, JDBC})
+@ActiveProfiles({HSQL_DB, JDBC})
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
 }
